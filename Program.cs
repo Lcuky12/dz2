89a29042d125fe137bd6cbab134cf73fe64d9102
Lcuky12 <118757873+Lcuky12@users.noreply.github.com>
2022-11-21 20:31:23 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float itog;
+            int a = (31 - 5 * 10);
+            itog = Convert.ToSingle(a) / 38;
+            Console.WriteLine(itog);
+        }
+    }
+}
